d629d5e1fd439ab3a29f2b61698368f01a5c970b
henrikschytze <henrikschytze@gmail.com>
2019-04-16 12:09:04 +0200

    should work now

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Starint build script"
-exec git clone https://github.com/Novicell/novicell-components.git tmp_src && mv -f ./tmp_src/* ./src && npm run fractal:build && npm run build:prod
+echo "Starting build script"
+exec git clone https://github.com/Novicell/novicell-components.git tmp_src 2>&1 | tee output.txt && cp -a -rf ./tmp_src/* ./src/ 2>&1 | tee output.txt && npm run fractal:build && npm run build:prod
