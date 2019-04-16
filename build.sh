17ad71779e734d273d86090819eb6adf1573fc2b
henrikschytze <henrikschytze@gmail.com>
2019-04-16 15:43:18 +0200

    added remove statement to bash

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Starting build script"
-exec git clone https://github.com/Novicell/novicell-components.git tmp_src 2>&1 | tee output.txt && cp -a -rf ./tmp_src/* ./src/ 2>&1 | tee output.txt && npm run fractal:build && npm run build:prod
+exec git clone https://github.com/Novicell/novicell-components.git tmp_src 2>&1 | tee output.txt && cp -a -rf ./tmp_src/* ./src/ 2>&1 | tee output.txt && rm -rf ./tmp_src 2>&1 | tee output.txt && npm run fractal:build && npm run build:prod
