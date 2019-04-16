cf0c4046a2de00d8754bac2564490d5883313ea2
henrikschytze <henrikschytze@gmail.com>
2019-04-16 10:58:52 +0200

    attempting with new directory structure

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "Starint build script"
-exec git clone https://github.com/Novicell/novicell-components.git ./tmp_src && mv -f ./tmp_src/* ./src && npm run fractal:build && npm run build:prod
-#!/bin/bash
+exec git clone https://github.com/Novicell/novicell-components.git tmp_src && mv -f ./tmp_src/* ./src && npm run fractal:build && npm run build:prod
