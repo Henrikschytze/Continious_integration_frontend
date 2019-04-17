0010520d9d18aabf62e1cba3d0fbb0c66f6069f9
henrikschytze <henrikschytze@gmail.com>
2019-04-17 15:42:09 +0200

    new approach

--- src/Modules/app.js
+++ src/Modules/app.js
@@ -1,3 +1,8 @@
 // This is the global JS file that will be accessible from any component
-import {initAos} from "../02-molecules/animate-on-scroll-wrapper/animate-on-scroll-wrapper.js";
+'use strict';
+import AOS from 'aos';
+
+function initAos(){
+    AOS.init({});
+}
 initAos();
