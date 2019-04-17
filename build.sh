0d8b79f827f4dffb22bddbdde855145a2bd82cb9
henrikschytze <henrikschytze@gmail.com>
2019-04-17 08:16:46 +0200

    add comments

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Starting build script"
 exec git clone https://github.com/Novicell/novicell-components.git tmp_src 2>&1 | tee output.txt && cp -a -rf ./tmp_src/* ./src/ 2>&1 | tee output.txt && rm -rf ./tmp_src 2>&1 | tee output.txt && npm run fractal:build && npm run build:prod
+echo "Components copied to ./src"
