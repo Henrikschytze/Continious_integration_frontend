86116859314d4c0d895d315d08e12bde3bb5ec6e
henrikschytze <henrikschytze@gmail.com>
2019-04-17 14:27:01 +0200

    new shell function added

--- build.sh
+++ build.sh
@@ -5,9 +5,17 @@ clone_components()
 {
 echo "Starting clone and build scripts"
 git clone https://github.com/Henrikschytze/novicell-components.git tmp_src 
-cp -a -rf ./tmp_src/* ./src/ 
+cp -a -rf ./tmp_src/* ./src/
+copyDependencies()
 rm -rf ./tmp_src
 echo "Components copied to ./src"
+}
+copy_dependencies()
+{
+echo "Attempting to add to package.json" 
+node ./src/copyDependencies.js 
+echo "installing new dependencies"
+npm install 
 }
 #Function for running actual build commands
 build_site()
