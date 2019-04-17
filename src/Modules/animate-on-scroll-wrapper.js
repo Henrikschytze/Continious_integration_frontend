f110e55a192b0ce1250b34a82703545d6501e5b3
henrikschytze <henrikschytze@gmail.com>
2019-04-17 15:45:00 +0200

    new approach

---
+++ src/Modules/animate-on-scroll-wrapper.js
@@ -0,0 +1,3 @@
+function initAos(){
+    AOS.init({});
+}
--- src/Modules/app.js
+++ src/Modules/app.js
@@ -1,8 +1,1 @@
 // This is the global JS file that will be accessible from any component
-'use strict';
-import AOS from 'aos';
-
-function initAos(){
-    AOS.init({});
-}
-initAos();
