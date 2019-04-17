aa193eb45800117349e032625043b2048157d5b1
henrikschytze <henrikschytze@gmail.com>
2019-04-17 15:08:23 +0200

    trying colors

--- build.sh
+++ build.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 set -e
+#Color variable green
+GREEN='tput setaf 2'
 #Function for cloning the repo and copying it into the src
 clone_components()
 {
-echo "Starting clone and build scripts"
+echo "${GREEN}Starting clone and build scripts"
 git clone https://github.com/Henrikschytze/novicell-components.git tmp_src 
 cp -a -rf ./tmp_src/* ./src/
 copy_dependencies
 rm -rf ./tmp_src
-echo "Components copied to ./src"
+echo "${GREEN}Components copied to ./src"
 }
 copy_dependencies()
 {
-echo "Attempting to add to package.json" 
+echo "${GREEN}Attempting to add to package.json" 
 node ./src/copyDependencies.js 
-echo "installing new dependencies"
+echo "${GREEN}installing new dependencies..."
 npm install 
 }
 #Function for running actual build commands
 build_site()
 { 
-echo "Building site"
+echo "${GREEN}Building site"
 npm run build:prod
 npm run fractal:build 
 }
