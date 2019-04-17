7d753abb8aca4674b1df5720970a17429ce216e8
henrikschytze <henrikschytze@gmail.com>
2019-04-17 08:35:56 +0200

    testing without ands

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "Starting build script"
-git clone https://github.com/Novicell/novicell-components.git tmp_src && cp -a -rf ./tmp_src/* ./src/ && rm -rf ./tmp_src && npm run fractal:build && npm run build:prod
+exec git clone https://github.com/Novicell/novicell-components.git tmp_src 
+exec cp -a -rf ./tmp_src/* ./src/ 
+exec rm -rf ./tmp_src 
+exec npm run fractal:build 
+exec npm run build:prod
 echo "Components copied to ./src"
