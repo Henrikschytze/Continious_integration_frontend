e74c50c85bd67a71cf68e9c11b0b8e75b8e4ad12
henrikschytze <henrikschytze@gmail.com>
2019-04-17 08:41:20 +0200

    trying with function instead

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+build_site()
+{
+git clone https://github.com/Novicell/novicell-components.git tmp_src 
+cp -a -rf ./tmp_src/* ./src/ 
+rm -rf ./tmp_src 
+npm run fractal:build 
+npm run build:prod
+}
 echo "Starting build script"
-exec git clone https://github.com/Novicell/novicell-components.git tmp_src 
-exec cp -a -rf ./tmp_src/* ./src/ 
-exec rm -rf ./tmp_src 
-exec npm run fractal:build 
-exec npm run build:prod
+build_site
 echo "Components copied to ./src"
