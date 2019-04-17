40a7cca1a4f8dd9626eb76b723c1a425fa8a5517
Henrik Schütze <33839074+Henrikschytze@users.noreply.github.com>
2019-04-17 10:52:56 +0200

    Developing (#1)
    * updated toml file
    
    * attempting with shell script
    
    * attempting with minor change
    
    * attempt with linux eol and exec
    
    * changed order of operations
    
    * back to toml
    
    * testing with minimal specs
    
    * changed to chmod as well
    
    * Attempting again
    
    * trying with two commands
    
    * It worked on previous commit, now trying with git clone
    
    * trying with quotes
    
    * build script with echo
    
    * trying with new command
    
    * attempting with forced overwrite
    
    * attempting with new directory structure
    
    * should work now
    
    * added remove statement to bash
    
    * checking with css files again
    
    * trying with different css setup
    
    * Attempting with different css setup...
    
    * add comments
    
    * removed text outputs
    
    * testing without exec
    
    * testing without ands
    
    * trying with function instead
    
    * removed video-fullscreen and changed order of bsh functions
    
    * better commenting
    
    * changed the path in preview hbs
    
    * revert preview hbs
    
    * changed order of operations in toml file
    
    * changed build publish
    
    * reverted toml order and added copy of dist to shell script
    
    * altered fractal.js dist path
    
    * trying with different fractal.js
    
    * changed order of build
    
    * added temp local repository

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+clone_components()
+{
+echo "Starting clone and build scripts"
+git clone https://github.com/Henrikschytze/novicell-components.git tmp_src 
+cp -a -rf ./tmp_src/* ./src/ 
+rm -rf ./tmp_src
+echo "Components copied to ./src"
+build_site 
+}
+build_site()
+{ 
+echo "Building site"
+npm run build:prod
+npm run fractal:build 
+}
+clone_components
