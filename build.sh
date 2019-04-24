5fd332e0c0191e771ea60e1d95eb133c038133e9
Henrik Schütze <33839074+Henrikschytze@users.noreply.github.com>
2019-04-24 08:21:08 +0200

    Developing (#2)
    * updated toml file
    
    * attempting with shell script
    
    * attempting with minor change
    
    * attempt with linux eol and exec
    
    * changed order of operations
    
    * back to toml
    
    * testing with minimal specs
    
    * changed to chmod as well
    
    * Attempting again
    
    * trying with two commands
    
    * It worked on previous commit, now trying with git clone
    
    * trying with quotes
    
    * build script with echo
    
    * trying with new command
    
    * attempting with forced overwrite
    
    * attempting with new directory structure
    
    * should work now
    
    * added remove statement to bash
    
    * checking with css files again
    
    * trying with different css setup
    
    * Attempting with different css setup...
    
    * add comments
    
    * removed text outputs
    
    * testing without exec
    
    * testing without ands
    
    * trying with function instead
    
    * removed video-fullscreen and changed order of bsh functions
    
    * better commenting
    
    * changed the path in preview hbs
    
    * revert preview hbs
    
    * changed order of operations in toml file
    
    * changed build publish
    
    * reverted toml order and added copy of dist to shell script
    
    * altered fractal.js dist path
    
    * trying with different fractal.js
    
    * changed order of build
    
    * added temp local repository
    
    * Made it easier to read shell file
    
    * new shell function added
    
    * syntax error fix
    
    * trying colors
    
    * no colors
    
    * trying import in app.js
    
    * new approach
    
    * new approach
    
    * revert changes before going home

--- build.sh
+++ build.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 set -e
-
+#Function for cloning the repo and copying it into the src
 clone_components()
 {
 echo "Starting clone and build scripts"
 git clone https://github.com/Henrikschytze/novicell-components.git tmp_src 
-cp -a -rf ./tmp_src/* ./src/ 
+cp -a -rf ./tmp_src/* ./src/
+copy_dependencies
 rm -rf ./tmp_src
 echo "Components copied to ./src"
-build_site 
 }
+copy_dependencies()
+{
+echo "Attempting to add to package.json" 
+node ./src/copyDependencies.js 
+echo "installing new dependencies"
+npm install 
+}
+#Function for running actual build commands
 build_site()
 { 
 echo "Building site"
@@ -17,3 +25,4 @@ npm run build:prod
 npm run fractal:build 
 }
 clone_components
+build_site 
