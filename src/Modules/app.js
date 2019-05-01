25037b4849be5d51a01fc6025fbfa19757cd599c
henrikschytze <henrikschytze@gmail.com>
2019-05-01 15:28:42 +0200

    reverting to hello world

--- src/Modules/app.js
+++ src/Modules/app.js
@@ -1,11 +1,14 @@
-// This is the global JS file that will be accessible from any component
+// // This is the global JS file that will be accessible from any component
 // import 'novicell-lazyload';
-import '../03-organisms/pageheaders/pageheader-slider/pageheader-slider';
+// import '../03-organisms/pageheaders/pageheader-slider/pageheader-slider';
 // import '../03-organisms/pageheaders/pageheader-dots/pageheader-dots';
 // import '../02-molecules/navigation/nav-main-list-priority/nav-main-list-priority';
 // import '../03-organisms/topbar/topbar-related/topbar-related';
 // import '../02-molecules/navigation/nav-trigger/nav-trigger';
 // import '../03-organisms/pageheaders/pageheader-video/pageheader-video--youtube';
+import { helloWorld } from 'hello-world-npm';
+
+console.log(helloWorld());
 // import NovicellLazyLoad from 'novicell-lazyload';
 // import debounce from 'lodash/debounce';
 
