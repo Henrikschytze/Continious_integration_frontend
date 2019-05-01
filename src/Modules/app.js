e8f94c4a32792b3fcaa2f5e05d7b790caea0da27
henrikschytze <henrikschytze@gmail.com>
2019-05-01 13:16:50 +0200

    trying with one import statemnt

--- src/Modules/app.js
+++ src/Modules/app.js
@@ -1,7 +1,7 @@
 // This is the global JS file that will be accessible from any component
 // import 'novicell-lazyload';
 console.log("from app.js");
-// import '../03-organisms/pageheaders/pageheader-dots/pageheader-dots';
+import '../03-organisms/pageheaders/pageheader-dots/pageheader-dots';
 // import '../02-molecules/navigation/nav-main-list-priority/nav-main-list-priority';
 // import '../03-organisms/topbar/topbar-related/topbar-related';
 // import '../02-molecules/navigation/nav-trigger/nav-trigger';
