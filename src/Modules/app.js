2798683b6f889c5619eeabc9c78d5411d0f7909c
henrikschytze <henrikschytze@gmail.com>
2019-05-02 15:41:09 +0200

    trying only with the organism js

--- src/Modules/app.js
+++ src/Modules/app.js
@@ -2,11 +2,11 @@
 // import 'novicell-lazyload';
 
 
-import '../03-organisms/pageheaders/pageheader-slider/pageheader-slider';
+// import '../02-molecules/navigation/nav-trigger/nav-trigger';
+// import '../02-molecules/navigation/nav-main-list-priority/nav-main-list-priority';
+// import '../03-organisms/pageheaders/pageheader-slider/pageheader-slider';
 import '../03-organisms/pageheaders/pageheader-dots/pageheader-dots';
-import '../02-molecules/navigation/nav-main-list-priority/nav-main-list-priority';
 import '../03-organisms/topbar/topbar-related/topbar-related';
-import '../02-molecules/navigation/nav-trigger/nav-trigger';
 import '../03-organisms/pageheaders/pageheader-video/pageheader-video--youtube';
 
 // import NovicellLazyLoad from 'novicell-lazyload';
