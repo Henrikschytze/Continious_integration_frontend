4cc359a024d9dc94271179d8b37c25c52e5cbb38
henrikschytze <henrikschytze@gmail.com>
2019-05-15 10:12:02 +0200

    Trying commands for adding assets now

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ set -e
 clone_components()
 {
 git clone https://github.com/Henrikschytze/novicell-components.git tmp_src 
+echo "Copying asset folders from tmp_src to ./assets/"
+cp -a -rf ./tmp_src/assets/* ./assets/
+# Remove the temp assets so they're not copied alongside the other contents of the components dir
+rm -rf ./tmp_src/assets/
+# Copy all the remaining contents of tmp_src to ./src/
 cp -a -rf ./tmp_src/* ./src/
 # Copying master.css and app.js from components to main dir
 echo "Overriding inner contents from master_modules/app.js and master.css to src/Modules"
